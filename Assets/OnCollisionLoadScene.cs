4060171cc1fd3ee9e4a75cc222b4e552cb9890e1
pascal <pascal3benschop@gmail.com>
2022-12-12 14:47:11 +0100

    Clean up

--- Assets/OnCollisionLoadScene.cs
+++ Assets/OnCollisionLoadScene.cs
@@ -7,42 +7,11 @@ using UnityEngine.Events;
 
 public class OnCollisionLoadScene : MonoBehaviour
 {
-    public UnityEvent eventz;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    void onCollisionEnter(Collision collision)
-    {
-        Debug.Log("On Collision");
-    }
-
 	void OnTriggerEnter(Collider other) {
 		if(other.gameObject.CompareTag("Player")) {
-			eventz.Invoke();
 			Debug.Log("Entered Area Scene");
             SceneManager.LoadSceneAsync ("SampleScene");
 
 		}
 	}
-
-    // void OnTriggerEnter(Collider collider)
-    // {
-    //     Debug.Log("Colided");
-    //     if (collider.gameObject.tag == "Player")
-    //     {
-    //         SceneManager.LoadScene ("SampleScene");
-    //         //Or:
-    //         //SceneManager.LoadScene (SceneIndex); //(without these: ", because it's a number - an int, not a string)
-    //     }
-    // }
 }
