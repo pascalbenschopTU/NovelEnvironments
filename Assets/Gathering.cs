ec99a2a897727a8cbd5c3486f80102a3d6c3cd63
andrejus77 <andrejkill77@gmail.com>
2023-01-15 22:45:47 +0100

    Fixed gathering issues

--- Assets/Gathering.cs
+++ Assets/Gathering.cs
@@ -10,12 +10,18 @@ public class Gathering : MonoBehaviour
 
     private enum UpDown { Down = -1, Start = 0, Up = 1 };
     private Text text;
-    string shownText = "Press E to collect lantern";
+    private Text text2;
+    string baseText2 = "Lanterns ";
+    string baseText = "Press E to Collect Lantern ";
+    string shownText;
     float raycastDistance = 5;
+    int itemsToCollect = 3;
+    int itemsCollected = 0;
 
     void Start()
     {
         environmentConfiguration = ExperimentMetaData.currentEnvironment;
+        shownText = "Press E to Collect Lantern 0/" + itemsToCollect.ToString();
         // Load the Arial font from the Unity Resources folder.
         Font arial;
         arial = (Font)Resources.GetBuiltinResource(typeof(Font), "Arial.ttf");
@@ -50,6 +56,27 @@ public class Gathering : MonoBehaviour
         rectTransform = text.GetComponent<RectTransform>();
         rectTransform.localPosition = new Vector3(0, 0, 0);
         rectTransform.sizeDelta = new Vector2(600, 200);
+
+        // Create the Text GameObject.
+        GameObject text2GO = new GameObject();
+        text2GO.transform.parent = canvasGO.transform;
+        text2GO.AddComponent<Text>();
+
+        // Set Text component properties.
+        text2 = text2GO.GetComponent<Text>();
+        text2.font = arial;
+        text2.text = "Lanterns 0/" + itemsToCollect.ToString();
+        text2.fontSize = 48;
+        text2.color = Color.black;
+        text2.alignment = TextAnchor.UpperLeft;
+
+        // Provide Text position and size using RectTransform.
+        RectTransform rectTransform2;
+        rectTransform2 = text2.GetComponent<RectTransform>();
+        //rectTransform2.localPosition = new Vector3(0, 0, 0);
+        rectTransform2.sizeDelta = new Vector2(600, 200);
+        rectTransform2.anchoredPosition = new Vector2(-600, 350);
+        System.Console.WriteLine("anchoredPosition: {0}", rectTransform2.anchoredPosition);
     }
 
     void Update()
@@ -60,9 +87,14 @@ public class Gathering : MonoBehaviour
         if (Physics.Raycast(ray, out hit, raycastDistance))
         {
             if (hit.collider.CompareTag("Gather")) {
-                if (Input.GetKeyDown(KeyCode.E)){
-                    shownText = "Lantern collected";
+                if (itemsCollected == itemsToCollect) {
+                    shownText = "All Lanterns Collected";
                     text.color = Color.magenta;
+                } else if (Input.GetKeyDown(KeyCode.E)) {
+                    itemsCollected++;
+                    shownText = baseText + itemsCollected.ToString() + "/" + itemsToCollect.ToString();
+                    text2.text = baseText2 + itemsCollected.ToString() + "/" + itemsToCollect.ToString();
+                    Destroy(hit.collider.gameObject);
                     LogGathering();
                 }
                 text.text = shownText;
