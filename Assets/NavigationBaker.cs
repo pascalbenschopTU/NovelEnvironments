2c527e7bd1d85af26f6ef4fa3c6b18e7b69607a2
Andrei <andrew.popovici9@gmail.com>
2023-01-17 21:10:54 +0100

    landmarks for environment1 added

---
+++ Assets/NavigationBaker.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavigationBaker : MonoBehaviour
+{
+
+    // public NavMeshSurface[] surfaces;
+    // // Start is called before the first frame update
+    // void Start()
+    // {
+    //     for (int i = 0; i < surfaces.length; i++)
+    //     {
+    //         surfaces[i].BuildNavMesh();
+    //     }
+    // }
+}
