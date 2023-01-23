7058c393c74c0cf3b958978c023e204ae86cfaa3
pascalbenschopTU <pascal3benschop@gmail.com>
2023-01-23 10:56:16 +0100

    Remove build breaking line and make minimap see more

--- Assets/ScriptsLogUser/RoamingEntropy.cs
+++ Assets/ScriptsLogUser/RoamingEntropy.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using UnityEngine;
 using UnityEngine.Internal;
-using Directory = UnityEngine.Windows.Directory;
+//using Directory = UnityEngine.Windows.Directory;
 
 namespace ScriptsLogUser
 {
